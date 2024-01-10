ab80ee0718955d33af5011c0c90639e49e40b3e9
mimi0000oo <mimiionescu07@gmail.com>
2024-01-10 10:49:46 +0000

    remove double comments?

--- home/programs/vscodium.nix
+++ home/programs/vscodium.nix
@@ -6,7 +6,7 @@
     enable = true;
     package = pkgs.vscodium;
     userSettings = {
-    #  # "clangd.path" = "/home/mimi07/.config/VSCodium/User/globalStorage/llvm-vs-code-extensions.vscode-clangd/install/17.0.3/clangd_17.0.3/bin/clangd";
+    #  "clangd.path" = "/home/mimi07/.config/VSCodium/User/globalStorage/llvm-vs-code-extensions.vscode-clangd/install/17.0.3/clangd_17.0.3/bin/clangd";
      "editor.tabSize" = 2;
      "window.titleBarStyle" = "custom";
      "workbench.colorTheme" = "Catppuccin Mocha";
