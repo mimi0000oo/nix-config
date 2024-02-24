22cc0cfe2ae880e3bfcc57f4061dbc93ad9223e9
mimi0000oo <mimiionescu07@gmail.com>
2024-02-25 00:11:02 +0200

    minor kernel & battery changes

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,28 @@
   #   # If using dhcpcd:
   #   dhcpcd.extraConfig = "nohook resolv.conf";
   # };
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
+  security.polkit.enable = true;
+
+  powerManagement.enable = true;
+  services.thermald.enable = true;
+  services.tlp = {
+    enable = true;
+    # settings = {
+    #   CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    #   CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+    #   CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    #   CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+    # };
+  };
+
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
